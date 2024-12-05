58b48b9df533f9a147c2cb043b7b4d282b3ed59a
Alex Freeman <143981358+thisisnotalex500@users.noreply.github.com>
2024-12-05 10:50:05 -0600

    Add project files.

---
+++ Clown/Clown.cs
@@ -0,0 +1,14 @@
+﻿// These statements create an instance of 
+// Clown and then set its fields
+
+internal class Clown
+{
+    internal string name;
+
+    public int Height { get; internal set; }
+
+    internal void TalkAboutYourself()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Clown/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // These statements create an instance of 
+        // Clown and then set its fields
+        Clown oneClown = new Clown
+        {
+            name = "Boffo",
+            Height = 14
+        };
+        oneClown.TalkAboutYourself();
+
+        // These statements instantiate a second
+        // Clown object and fill it with data.
+
+        Clown anotherClown = new Clown();
+        anotherClown.name = "Biff";
+        anotherClown.Height = 16;
+        anotherClown.TalkAboutYourself();
+
+        // Now we instantiate a third Clown object
+        // and use data from the other two
+        // instances to set its fields
+        Clown clown3 = new Clown();
+        clown3.name = anotherClown.name;
+        clown3.Height = oneClown.Height - 3;
+        clown3.TalkAboutYourself();
+
+        // Notice how there's no "new" statement
+        // here -- we're not creating a new object,
+        // just modifying one already in memory
+        anotherClown.Height *= 2;
+        anotherClown.TalkAboutYourself();
+    }
+
+}
